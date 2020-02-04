8a6833180c1530ac6dcc222bd4f9b887ab21ea53
Keith Tachibana <Keith.Tachibana@gmail.com>
2020-02-04 00:28:57 -0800

    Removed contact button from all pages and added contact info to nav bar, made the background color dark for all pages, and added some pictures to the 'About Me' section

--- assets/js/about.js
+++ assets/js/about.js
@@ -14,3 +14,20 @@ const close_spanish_bar = document.getElementById('close_spanish_bar');
 close_spanish_bar.onclick = function () {
   modalbox_spanish_bar.style.display = "none";
 }
+
+// Greenpoint Brooklyn
+const modalbox_greenpoint_brooklyn = document.getElementById('modalbox_greenpoint_brooklyn');
+const greenpoint_brooklyn = document.getElementById('greenpoint_brooklyn');
+const modal_greenpoint_brooklyn = document.getElementById('modal_greenpoint_brooklyn');
+const caption_greenpoint_brooklyn = document.getElementById('caption_greenpoint_brooklyn');
+
+greenpoint_brooklyn.onclick = function () {
+  modalbox_greenpoint_brooklyn.style.display = "block";
+  modal_greenpoint_brooklyn.src = this.src;
+  caption_greenpoint_brooklyn.innerHTML = this.alt;
+}
+
+const close_greenpoint_brooklyn = document.getElementById('close_greenpoint_brooklyn');
+close_greenpoint_brooklyn.onclick = function () {
+  modalbox_greenpoint_brooklyn.style.display = "none";
+}
