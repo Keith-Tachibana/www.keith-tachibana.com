8b3e8791681c4fafa529853953fddfc1543bc1ac
Keith Tachibana <Keith.Tachibana@gmail.com>
2020-03-01 21:04:55 -0800

    Refactored Homepage To Do List project

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,5 @@
 const formWeather = document.getElementById('weather-form');
 const formHeadlines = document.getElementById('headlines-form');
-const formSports = document.getElementById('sports-form');
 const formGiphy = document.getElementById('giphy-form');
 
 const weather = new Weather(formWeather);
@@ -9,10 +8,9 @@ const greeting = new Greeting();
 const newDate = new NewDate();
 const clock = new Clock();
 const news = new News(formHeadlines);
-const sports = new Sports(formSports)
 const stocks = new Stocks();
 const giphy = new Giphy(formGiphy);
 const quotes = new Quotes();
-const app = new App(weather, nasa, greeting, newDate, clock, news, sports, stocks, giphy, quotes);
+const app = new App(weather, nasa, greeting, newDate, clock, news, stocks, giphy, quotes);
 
 app.start();
--- assets/js/homepageAPI/maps.js
+++
@@ -1,8 +0,0 @@
-var map;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById('maps'), {
-    center: {lat: 33.632921, lng: -117.810891},
-    zoom: 14
-  });
-};
