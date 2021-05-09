adca0ee96ec507792ef5f4ea039f6c086672e3f7
Keith Tachibana <Keith.Tachibana@gmail.com>
2021-05-09 00:32:16 -0700

    Removed SGT from portfolio folder

--- portfolio/studentGradeTable/data.sql
+++
@@ -1,6 +0,0 @@
-truncate table "grades" restart identity;
-
-insert into "grades" ("name", "course", "grade")
-values ('Simon Peyton Jones', 'Haskell', 100),
-       ('Barbara Liskov', 'CLU', 100),
-       ('Rasmus Lerdorf', 'PHP', 100);
