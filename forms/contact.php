b06ab7bc0a2f9a6a5699e09eef6ffefe09dd1dd1
Keith Tachibana <Keith.Tachibana@gmail.com>
2021-10-21 12:52:05 -0700

    Revamped recaptcha

--- forms/contact.php
+++ forms/contact.php
@@ -1,44 +1,90 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+// require ReCaptcha class
+require('recaptcha-master/src/autoload.php');
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'Keith.Tachibana@gmail.com';
+// configure
+// an email address that will be in the From field of the email.
+$from = 'webmaster@silvercrescent.org';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+// an email address that will receive the email with the output of the form
+$sendTo = 'Keith.Tachibana@gmail.com';
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
+// subject of the email
+$subject = 'New message from contact form';
 
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+// form field names and their translations.
+// array variable name => Text to appear in the email
+$fields = array('name' => 'Name', 'email' => 'Email', 'subject' => 'Subject', 'message' => 'Message');
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+// message that will be displayed when everything is OK :)
+$okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
 
-  $contact->smtp = array(
-    'host' => 'smtp.gmail.com',
-    'username' => 'Keith.Tachibana@gmail.com',
-    'password' => 'bawoxqaxqtjpwair',
-    'port' => '587'
-  );
+// If something goes wrong, we will display this message.
+$errorMessage = 'There was an error while submitting the form. Please try again later';
 
+// ReCaptch Secret
+$recaptchaSecret = '6LdIy9EaAAAAAPQJlef78IKYWqowox1AQ5FVeLkb';
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+// let's do the sending
 
-  $contact->honeypot = $_POST['first_name'];
-  $contact->recaptcha_secret_key = '6LeKMdIaAAAAADHaDB1toMMJt_YGcG5wTJO3yDcX';
+// if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
+error_reporting(E_ALL & ~E_NOTICE);
 
-  echo $contact->send();
-?>
+try {
+    if (!empty($_POST)) {
+
+        // validate the ReCaptcha, if something is wrong, we throw an Exception,
+        // i.e. code stops executing and goes to catch() block
+        
+        if (!isset($_POST['g-recaptcha-response'])) {
+            throw new \Exception('ReCaptcha is not set.');
+        }
+
+        // do not forget to enter your secret key from https://www.google.com/recaptcha/admin
+        
+        $recaptcha = new \ReCaptcha\ReCaptcha($recaptchaSecret, new \ReCaptcha\RequestMethod\CurlPost());
+        
+        // we validate the ReCaptcha field together with the user's IP address
+        
+        $response = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
+
+        if (!$response->isSuccess()) {
+            throw new \Exception('ReCaptcha was not validated.');
+        }
+        
+        // everything went well, we can compose the message, as usually
+        
+        $emailText = "You have a new message from your contact form\n=============================\n";
+
+        foreach ($_POST as $key => $value) {
+            // If the field exists in the $fields array, include it in the email
+            if (isset($fields[$key])) {
+                $emailText .= "$fields[$key]: $value\n";
+            }
+        }
+    
+        // All the neccessary headers for the email.
+        $headers = array('Content-Type: text/plain; charset="UTF-8";',
+            'From: ' . $from,
+            'Reply-To: ' . $from,
+            'Return-Path: ' . $from,
+        );
+        
+        // Send email
+        mail($sendTo, $subject, $emailText, implode("\n", $headers));
+
+        $responseArray = array('type' => 'success', 'message' => $okMessage);
+    }
+} catch (\Exception $e) {
+    $responseArray = array('type' => 'danger', 'message' => $e->getMessage());
+}
+
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    $encoded = json_encode($responseArray);
+
+    header('Content-Type: application/json');
+
+    echo $encoded;
+} else {
+    echo $responseArray['message'];
+}
